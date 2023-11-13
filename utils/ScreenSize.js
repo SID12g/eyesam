25ba58e57804ce24aec55b586a72764745098a7a
SID12g <sid12g@naver.com>
2023-11-13 22:29:53 +0900

    add :: bottom tab bar

--- utils/ScreenSize.js
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from 'react-native';
- 
-const windowWidth = Dimensions.get('window').width/393;
-const windowHeight = Dimensions.get('window').height/852;
-
-export {windowWidth, windowHeight}
